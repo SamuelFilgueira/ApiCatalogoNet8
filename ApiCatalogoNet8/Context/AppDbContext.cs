b3246d4aadcc1f366e43a027f5a2e7b707085401
Samuel Filgueira <leumas685@gmail.com>
2024-02-16 07:18:13 -0300

    Adicionar arquivos de projeto.

---
+++ ApiCatalogoNet8/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ApiCatalogoNet8.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogoNet8.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) :base(options) { }
+
+    DbSet<Produto>? Produtos { get; set; }
+    DbSet<Categoria>? Categorias { get; set; }
+}
