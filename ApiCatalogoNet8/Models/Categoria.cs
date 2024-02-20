3dc1c28df19bdb4904c515325dbefc0209adb68e
Samuel Filgueira <leumas685@gmail.com>
2024-02-20 06:56:53 -0300

    Definindo o relacionamento entre as entidadas

--- ApiCatalogoNet8/Models/Categoria.cs
+++ ApiCatalogoNet8/Models/Categoria.cs
@@ -1,8 +1,16 @@
-﻿namespace ApiCatalogoNet8.Models;
+﻿using System.Collections.ObjectModel;
+
+namespace ApiCatalogoNet8.Models;
 
 public class Categoria
 {
+    public Categoria() 
+    {
+        Produtos = new Collection<Produto>();
+    }  
+
     public int CategoriaId { get; set; }
     public string? Nome { get; set; }
     public string? ImagemUrl { get; set; }
+    public ICollection<Produto>? Produtos { get; set; }
 }
