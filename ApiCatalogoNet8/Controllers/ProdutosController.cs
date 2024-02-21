d4720b9f3d911f803824018904d091c0959f81fc
Samuel Filgueira <leumas685@gmail.com>
2024-02-21 09:36:00 -0300

    Iniciando criação dos controllers

---
+++ ApiCatalogoNet8/Controllers/ProdutosController.cs
@@ -0,0 +1,17 @@
+﻿using ApiCatalogoNet8.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalogoNet8.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
