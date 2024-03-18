2c984edf5c88c528b9f27474958bb21a950878ec
Samuel Filgueira <leumas685@gmail.com>
2024-03-18 12:10:52 -0300

    Criando o repositorio especifico.

---
+++ ApiCatalogoNet8/Repositories/CategoriaRepository.cs
@@ -0,0 +1,61 @@
+﻿using ApiCatalogoNet8.Context;
+using ApiCatalogoNet8.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogoNet8.Repositories;
+
+public class CategoriaRepository : ICategoriaRepository
+{
+    private readonly AppDbContext _context;
+
+    public CategoriaRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Categoria> GetCategorias()
+    {
+        var categorias = _context.Categorias.ToList();
+        return categorias;
+    }
+
+    public Categoria GetCategoria(int id)
+    {
+        var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+        return categoria;
+    }
+
+    public Categoria Create(Categoria categoria)
+    {
+        if(categoria == null)
+        {
+            throw new ArgumentNullException(nameof(categoria));
+        }
+        _context.Categorias.Add(categoria);
+        _context.SaveChanges();
+        return categoria;
+    }
+
+    public Categoria Update(Categoria categoria)
+    {
+        if(categoria == null)
+        {
+            throw new ArgumentNullException(nameof(categoria));
+        }
+        _context.Entry(categoria).State = EntityState.Modified;
+        _context.SaveChanges();
+        return categoria;
+    }
+
+    public Categoria Delete(int id)
+    {
+        var categoria = _context.Categorias.Find(id);
+        if (categoria == null)
+        {
+            throw new ArgumentNullException(nameof(categoria));
+        }
+        _context.Categorias.Remove(categoria);
+        _context.SaveChanges();
+        return categoria;
+    }
+}
---
+++ ApiCatalogoNet8/Repositories/ICategoriaRepository.cs
@@ -0,0 +1,12 @@
+﻿using ApiCatalogoNet8.Models;
+
+namespace ApiCatalogoNet8.Repositories;
+
+public interface ICategoriaRepository
+{
+    IEnumerable<Categoria> GetCategorias();
+    Categoria GetCategoria(int id);
+    Categoria Create(Categoria categoria);
+    Categoria Update(Categoria categoria);
+    Categoria Delete(int id);   
+}
